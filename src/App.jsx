4eb63f34dcfc7180bb87ccb384c22b75b7ab3e86
N1XTEN <130859559+N1XTEN@users.noreply.github.com>
2024-05-17 19:31:59 +0300

    clear app

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,22 @@ import SearchBox from './components/SearchBox/SearchBox';
 import ContactForm from "./components/ContactForm/ContactForm";
 
 const App = () => {
- const [contacts, setContacts] = useState(() => {
-    const savedContacts = window.localStorage.getItem('contacts');
-    return savedContacts ? JSON.parse(savedContacts) : [];
-  });
+  const [contacts, setContacts] = useState(() => JSON.parse(localStorage.getItem('contacts')) || []);
   const [filter, setFilter] = useState('');
 
-  const handleFilterChange = (e) => {
-    setFilter(e.target.value);
-  };
-
-  const addContact = (newContact) => {
-    const updatedContacts = [...contacts, { ...newContact, id: generateId() }];
-    setContacts(updatedContacts);
-  };
-
-  const deleteContact = (id) => {
-    const updatedContacts = contacts.filter((contact) => contact.id !== id);
-    setContacts(updatedContacts);
-  };
-
-  const generateId = () => {
-    return Math.random().toString(36).substr(2, 9);
-  };
-  
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  const filteredContacts = contacts.filter((contact) =>
+  const addContact = (newContact) => {
+    setContacts([...contacts, { ...newContact, id: Math.random().toString(36).substr(2, 9) }]);
+  };
+
+  const deleteContact = (id) => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
+
+  const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 
@@ -40,7 +27,7 @@ const App = () => {
     <div>
       <h1>Phonebook</h1>
       <ContactForm addContact={addContact} />
-      <SearchBox value={filter} onChange={handleFilterChange} />
+      <SearchBox value={filter} onChange={(e) => setFilter(e.target.value)} />
       <ContactList contacts={filteredContacts} deleteContact={deleteContact} />
     </div>
   );
